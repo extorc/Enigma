cf00f8aacb4078f0fd8b683c17afdc2febe0ad47
extorc <mittalshaurya004.com>
2023-03-05 19:52:44 +0530

    Sphere position added | Sexy comments added

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,22 @@
 #include "glm/glm.hpp"
 #include <vector>
 
-glm::vec4 processPixel(glm::vec3 rayOrigin, glm::vec3 rayDirection){
+glm::vec4 processPixel(glm::vec3 rayOrigin, glm::vec3 rayDirection, glm::vec3 objectPosition){
 	float a = glm::dot(rayDirection, rayDirection);
-	float b = 2 * glm::dot(rayDirection, rayOrigin);
-	float c = glm::dot(rayOrigin, rayOrigin) - 29.0f;
+	float b = 2 * glm::dot(rayDirection, rayOrigin - objectPosition);
+	float c = glm::dot(rayOrigin, rayOrigin) - 81.0f - 2 * glm::dot(rayOrigin, objectPosition) + glm::dot(objectPosition, objectPosition);
 
+	/*Quadratic equation of the sphere is foudn to be
+
+		Ax^2 + Bx^2T^2 + 2AxBxt + g^2 - 2gAx - 2gBxt +
+		Ay^2 + By^2T^2 + 2AyByt + h^2 - 2hAy - 2hByt + 
+		Az^2 + Bz^2T^2 + 2AzBzt + i^2 - 2zAz - 2iBzt - Radius ^ 2
+		  
+     ^        ^         ^         ^         ^       ^ 
+		 |        |         |         |         |       | 
+	const	  T^2 term    T term    const     T term   const
+                                                   
+		*/                                             
 	float discriminant = (b * b) - 4 * a * c;
 
 	if(discriminant >= 0){
@@ -44,12 +55,13 @@ int main(){
 	int image_width = window.width;
 
 	glm::vec3 origin(0.0f, 0.0f, -5.4f);
+	glm::vec3 sphere(0.0f, 0.0f, 9.0f);
 
 	for(int j = 0; j < image_height; j++){
 		for(int i = 0; i < image_width; i++){
-			glm::vec3 pixel(i-250, j-250, 0);
+			glm::vec3 pixel(i-250, j-250, 129.4f); //Z coord is foud by hit and trial to be good enough for zooming in on the sphere at this potiion. It only affects field of view
 			glm::vec3 ray = pixel - origin;
-			pixels.push_back(processPixel(origin,glm::normalize(ray)));
+			pixels.push_back(processPixel(origin, glm::normalize(ray), sphere));
 		}
 	}
 

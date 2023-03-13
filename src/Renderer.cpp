c47e3024e4cc305334f614d41f8ead0f95a625dc
extorc <mittalshaurya004.com>
2023-03-13 20:55:51 +0530

    Sexy comments

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -2,19 +2,19 @@
 #define RAND(X) static_cast <float> (rand()) / static_cast <float> (RAND_MAX)
 
 HitData Renderer::trace(Ray& ray){
-	float closestT = FLT_MAX;
+	float closestT = FLT_MAX;                                                                        //Keeping track of Closest distance of collision and the object occupied at that distance
 	int closestSphereIndex = -1;
 
 	for(int i = 0; i < scene->objects.size(); i++){
-		float t = scene->objects[i]->intersect(ray);
+		float t = scene->objects[i]->intersect(ray);                                                   //Looping through all the objects in the scene and running their respective intersect functions
 		if(t < closestT && t > 0){
-			closestT = t;
+			closestT = t;                                                                                //Updating the closest distance and object in case we have a nearer candidate
 			closestSphereIndex = i;
 		}
 	}
 	if(closestSphereIndex < 0)
-		return miss();
-	return scene->objects[closestSphereIndex]->hit(ray, closestT, closestSphereIndex);
+		return miss();                                                                                 //If no close object was detected, run the global miss function
+	return scene->objects[closestSphereIndex]->hit(ray, closestT, closestSphereIndex);               //Otherwise return the data about this collision
 }
 
 HitData Renderer::miss(){
@@ -22,6 +22,7 @@ HitData Renderer::miss(){
 	data.objectIndex = -1;
 	return data;
 }
+
 glm::vec4 Renderer::processPixel(Ray ray){
 	glm::vec4 finalColor(0.0f);
 	int bounces = 5;
@@ -30,11 +31,19 @@ glm::vec4 Renderer::processPixel(Ray ray){
 
 	for(int i = 0; i < bounces; i++){
 		HitData data = trace(ray);
-		if(data.objectIndex < 0){
+		if(data.objectIndex < 0){                                                                      //If we didnt hit anything during the bounce race, just append the sky color
 			finalColor += skyBox * (float)std::pow(0.5, i);
 			break;
 		}
 		else{
+
+			/*
+			 Firstly, calculate the light intensity at the point by comparing normal orientation with light direction
+			 Update color based on object color and light intensity calculated above
+			 Correct for self collision in preparation for bounce
+			 Update ray direction by reflecting the ray at collision site + randomizing normal to introduce roughness before re-calling trace
+			 */
+			
 			float d = glm::max(glm::dot(data.normal, -light), 0.0f);
 			finalColor += glm::vec4(d * scene->materialList[scene->objects[data.objectIndex]->matIndex].Albedo, 1.0f) * (float)std::pow(0.5, i);
 			ray.origin = data.position + data.normal * 0.001f;
--- src/main.cpp
+++ src/main.cpp
@@ -22,18 +22,16 @@
 int main(){
 	Window window = createWindow();
 	Frame frame = createTexture();
-
 	Scene scene;
+	Camera camera({0, 0, 5}, 500, 500);
+	Renderer renderer(camera, &scene);
 
 	scene.objects.push_back(std::unique_ptr<Object>(new Plane({0, 0, 0}, {0, -1.5f, 0}, {0, 1, 0}, 0)));
 	scene.objects.push_back(std::unique_ptr<Object>(new Sphere({0, 0.5f, -1}, 2, 1)));
-
 	scene.materialList.push_back({0.1f, {0, 0, 1}});
 	scene.materialList.push_back({1.0f, {1, 0, 0.5f}});
 
-	Camera camera({0, 0, 5}, 500, 500);
-	Renderer renderer(camera, &scene);
-	std::vector<glm::vec4> pixels;
+	std::vector<glm::vec4> pixels;                                                                      //Create buffers for pixel and accumulation data
 	pixels.resize(camera.u * camera.v);
 	std::fill(pixels.begin(), pixels.end(), glm::vec4(0));
 	std::vector<glm::vec4> accumulation;
@@ -48,14 +46,14 @@ int main(){
 	while(!glfwWindowShouldClose(window.window)){
 		camera.cameraInput(window.window);	
 		for(int j = 0; j < image_height; j++){
-			for(int i = 0; i < image_width; i++){
-				Ray ray = {glm::normalize(camera.rayDirections[j * image_width + i]), camera.cameraPosition};
-				auto color = renderer.processPixel(ray);
-				accumulation[j * camera.u + i] += color;
-				pixels[j * camera.u + i] = accumulation[j * camera.u + i]/(float)sampleCount;
+			for(int i = 0; i < image_width; i++){                                                           //For every pixel on the screen
+				Ray ray = {glm::normalize(camera.rayDirections[j * image_width + i]), camera.cameraPosition}; //Generate a ray
+				auto color = renderer.processPixel(ray);                                                      //And process the ray
+				accumulation[j * camera.u + i] += color;                                                      //Accumulate the data processed
+				pixels[j * camera.u + i] = accumulation[j * camera.u + i]/(float)sampleCount;                 //And update pixel data based on accumulation and sample count
 			}
 		}
-		sampleCount++;
+		sampleCount++;                                                                                    //Update sample count
 		std::cout<<sampleCount<<std::endl;
 		camera.calculateRayDirections();
 		blitFrame(frame, image_width, image_height, pixels);
